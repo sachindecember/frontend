8b34a760df74e82da9f0db33214f91da4b1ec8b1
sachin <sachinjoseph7070@gmail.com>
2021-11-30 17:04:17 +0530

    project frtask2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatTabsModule} from '@angular/material/tabs';
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import { BillingaddressComponent } from './billingaddress/billingaddress.component';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent,
+    BillingaddressComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,MatTabsModule,MatFormFieldModule,MatCardModule,MatButtonModule,MatInputModule,
+    BrowserAnimationsModule,FlexLayoutModule, MatSelectModule, MatRadioModule, ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
